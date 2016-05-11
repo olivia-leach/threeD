1eac0323f95b8532b460576b1a4688588cfa926b
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-11 15:55:12 -0400

    make cube with rotating ability

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,94 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+// element: a jQuery object containing the DOM element to use
+// dimensions: the number of cubes per row/column (default 3)
+// background: the scene background colour
+function Rubik(element, dimensions, background) {
 
-// use require without a reference to ensure a file is bundled
-require('./example');
+  dimensions = dimensions || 3;
+  background = background || 0x303030;
+
+  var width = element.innerWidth(),
+      height = element.innerHeight();
+
+  var debug = false;
+
+  /*** three.js boilerplate ***/
+  var scene = new THREE.Scene(),
+      camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000),
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  renderer.setClearColor(background, 1.0);
+  renderer.setSize(width, height);
+  renderer.shadowMapEnabled = true;
+  element.append(renderer.domElement);
+
+  camera.position = new THREE.Vector3(-20, 20, 30);
+  camera.lookAt(scene.position);
+  THREE.Object3D._threexDomEvent.camera(camera);
+
+  /*** Lights ***/
+  scene.add(new THREE.AmbientLight(0xffffff));
+
+  /*** Camera controls ***/
+  var orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
+
+  /*** Debug aids ***/
+  if(debug) {
+    scene.add(new THREE.AxisHelper( 20 ));
+  }
+
+  // build cubes
+  var colours = [0xC41E3A, 0x009E60, 0x0051BA, 0xFF5800, 0xFFD500, 0xFFFFFF],
+      faceMaterials = colours.map(function(c) {
+        return new THREE.MeshLambertMaterial({ color: c , ambient: c });
+      }),
+      cubeMaterials = new THREE.MeshFaceMaterial(faceMaterials);
+
+  var cubeSize = 4,
+      spacing = 0.1;
+
+  var increment = cubeSize + spacing,
+      allCubes = [];
+
+  function newCube(x, y, z) {
+    var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize);
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
+    cube.castShadow = true;
+
+    cube.position = new THREE.Vector3(x, y, z);
+    cube.rubikPosition = cube.position.clone();
+
+    scene.add(cube);
+    allCubes.push(cube);
+  }
+
+  var positionOffset = (dimensions - 1) / 2;
+  for(var i = 0; i < dimensions; i ++) {
+    for(var j = 0; j < dimensions; j ++) {
+      for(var k = 0; k < dimensions; k ++) {
+
+        var x = (i - positionOffset) * increment,
+            y = (j - positionOffset) * increment,
+            z = (k - positionOffset) * increment;
+
+        newCube(x, y, z);
+      }
+    }
+  }
+
+  function render() {
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+
+  /*** Util ***/
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  //Go!
+  render();
+
+}
